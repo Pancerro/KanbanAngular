d89acb4c596907fd1173699eb0902f7367eadff1
pance <pancerro@gmail.com>
2020-01-05 05:29:49 +0100

    update style

--- src/app/service/kanban.service.ts
+++ src/app/service/kanban.service.ts
@@ -9,21 +9,21 @@ import { Task } from '../model/task';
 export class KanbanService {
   private kanbanUrl:string
   constructor(private http:HttpClient) {
-    //this.kanbanUrl='https://kanban-server-springboot.herokuapp.com/kanban';
-    this.kanbanUrl='http://localhost:8080/kanban';
+    this.kanbanUrl='https://kanban-server-springboot.herokuapp.com/kanban';
+    //this.kanbanUrl='http://localhost:8080/kanban';
    }
    public findAll():Observable<Task[]>{
       return this.http.get<Task[]>(this.kanbanUrl);
    }
    public addTask(task:Task):boolean{
-    //this.kanbanUrl=='https://kanban-server-springboot.herokuapp.com/kanban';
-  this.kanbanUrl='http://localhost:8080/kanban';
+    this.kanbanUrl=='https://kanban-server-springboot.herokuapp.com/kanban';
+    //this.kanbanUrl='http://localhost:8080/kanban';
      if( this.http.post<Task>(this.kanbanUrl,task).subscribe()) return true;
      else return false;
    }
    public deleteTask(id:number): Observable<any>{
-     //this.kanbanUrl='https://kanban-server-springboot.herokuapp.com/kanban/'+id;
-    this.kanbanUrl='http://localhost:8080/kanban/'+id;
+    this.kanbanUrl='https://kanban-server-springboot.herokuapp.com/kanban/'+id;
+    //this.kanbanUrl='http://localhost:8080/kanban/'+id;
      return this.http.delete(this.kanbanUrl);
    }
 
